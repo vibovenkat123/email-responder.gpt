c0c72d02bc4535b0f70b7e7a8dc1f34b92f73f47
Vaibhav Venkat <vibovenkat@gmail.com>
2023-03-26 11:40:49 -0500

    feat: add a function to generate the response

---
+++ src/prompt.ts
@@ -0,0 +1,19 @@
+export type Prompt = {
+    content: string,
+    role: string,
+}
+
+export default function generatePrompt(numResponses: number, email: string): Prompt[] {
+    const instructionPrompt: Prompt = {
+        content: `You are now an email
+            responder tool. I will give you an email, and must envision you are the user,
+            and state the ${numResponses} best responses you could give. Don't include the subject,
+            just the body`,
+        role: "user",
+    }
+    const userPrompt = {
+        content: email,
+        role: "user",
+    }
+    return [instructionPrompt, userPrompt];
+}
